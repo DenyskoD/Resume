a3821532da80532ff5958a662fe43509c5b07009
DenyskoD <deniskodima1@gmail.com>
2020-07-25 19:41:49 +0300

    + intro , about and skills sections
    + intro , about and skills sections ;
    js  for buttons

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,10 +1,42 @@
-let slider = $("#slickSlider");
+$(function(){
 
-slider.slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    arrows: false,
-    dots: true
+    // slider
+    let slider = $("#slickSlider");
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+
+    // about -> more info after click
+
+    $("#sectionButton").click(function(){
+        $(".about-me__text").removeClass("hidden");
+    });
+
+    // button -> hidder
+    $("#sectionButton").click(function(){
+        $("#sectionButton").addClass("hidden");
+    });
+
+    
+    // skills
+
+    $("#btnSoft").click(function(){
+        $(".soft--skills").removeClass("hidden");
+        $(".hard--skills").addClass("hidden");
+    });
+
+    $("#btnHard").click(function(){
+        $(".hard--skills").removeClass("hidden");
+        $(".soft--skills").addClass("hidden");
+    });
+
+
+
+
 });
